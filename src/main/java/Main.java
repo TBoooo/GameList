d6302d8d0f599d1fa991ce305511495ec697579e
yuehaibo <yuehaibo@bonc.com.cn>
2018-11-23 15:02:00 +0800

    完成了易乐游、云更新、顺网的更新列表爬取。

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
 import us.codecraft.webmagic.Spider;
 
 public class Main {
-    public static void main(String[] args){
-            String SWUrl="http://www.icafe8.com/frontEnd/frontGameUpdList.do";
-            String YGXUrl="http://www.yungengxin.net.cn/game/update";
-            Spider spider = Spider.create(new processor());
-            spider
-//                    .addUrl(SWUrl)
-                    .addUrl(YGXUrl)
-                    .run();
+    public static void main(String[] args) {
+        String SWUrl = "http://www.icafe8.com/frontEnd/frontGameUpdList.do";
+        String YGXUrl = "http://www.yungengxin.net.cn/game/update";
+        String YLYUrl = "http://www.yileyoo.com/game/list?page=1";
+        Spider spider = Spider.create(new processor());
+        spider.addUrl(SWUrl);
+        spider.addUrl(YGXUrl);
+        spider.addUrl(YLYUrl);
+        spider.run();
     }
 }
--- src/main/java/processor.java
+++ src/main/java/processor.java
@@ -1,42 +1,47 @@
+import org.apache.commons.lang3.StringUtils;
+import org.assertj.core.util.VisibleForTesting;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.processor.PageProcessor;
-import us.codecraft.webmagic.selector.CssSelector;
 import us.codecraft.webmagic.selector.JsonPathSelector;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class processor implements PageProcessor {
-    private String SWHelpUrl ="http://www.icafe8.com/.*";
+    private String SWHelpUrl = "http://www.icafe8.com/.*";
     private String YGXHelpUrl = "http://www.yungengxin.net.cn/game/update.*";
+    private String YLYHelpUrl = "http://www.yileyoo.com/game/list.*";
+    private String datePattern = "yyyy-MM-dd HH:mm:ss";
 
     public void process(Page page) {
         //处理顺网逻辑
-        if (page.getUrl().regex(SWHelpUrl).match()){
+        if (page.getUrl().regex(SWHelpUrl).match()) {
             int totalPage = Integer.parseInt(new JsonPathSelector("$.page.totalPage").select(page.getRawText()));
             //判断是否是第一次进入处理队列
-            if ("http://www.icafe8.com/frontEnd/frontGameUpdList.do".equals(page.getUrl().toString())){
+            if ("http://www.icafe8.com/frontEnd/frontGameUpdList.do".equals(page.getUrl().toString())) {
                 //判断页码，如果不止一页，则添加其余页面
-                System.out.println("第一次进入顺网处理逻辑。");
-                if (totalPage!=1) {
-                    for (int x=2;x<=totalPage;x++){
-                        String addUrl = "http://www.icafe8.com/frontEnd/frontGameUpdList.do?pageNum="+x+"&gameName=";
+                if (totalPage != 1) {
+                    for (int x = 2; x <= totalPage; x++) {
+                        String addUrl = "http://www.icafe8.com/frontEnd/frontGameUpdList.do?pageNum=" + x + "&gameName=";
                         page.addTargetRequest(addUrl);
                     }
                 }
             }
 
             List<String> list = new JsonPathSelector("$.page.dataList[*]").selectList(page.getRawText());
-            for (String temp:list){
-                GameBean gameBean=new GameBean();
-                String sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-                gameBean.setCompany(new JsonPathSelector(".company").select(temp).replace("[]",""));
+            for (String temp : list) {
+                GameBean gameBean = new GameBean();
+                String sdf = new SimpleDateFormat(datePattern).format(new Date());
+                gameBean.setCompany(new JsonPathSelector(".company").select(temp).replace("[]", ""));
                 gameBean.setGameName(new JsonPathSelector(".gameName").select(temp));
                 gameBean.setUpdateTime(new JsonPathSelector(".modifyTime").select(temp));
                 gameBean.setGameType(new JsonPathSelector(".pkgClass").select(temp));
@@ -45,19 +50,67 @@ public class processor implements PageProcessor {
                 gameBean.setSource("顺网");
                 gameBean.setHandleFlag("0");
                 gameBean.setCrawlerTime(sdf);
-                System.out.println(gameBean);
+                Dao.insert(gameBean);
+            }
+        }
+
+        //易乐游处理逻辑。
+        if (page.getUrl().regex(YLYHelpUrl).match()) {
+
+            Document doc = Jsoup.parse(page.getRawText());
+            int totalDetail  = doc.select("tbody  > tr").size();
+            //判断当前页面是否为15条数据，如果是则添加下一页
+            if (totalDetail==15){//http://www.yileyoo.com/game/list?page=2
+                int currentPage = getYLYCurrentPage(page.getUrl().toString());
+                String targetUrl =  "http://www.yileyoo.com/game/list?page=" + (currentPage+1);
+                page.addTargetRequest(targetUrl);
+            }
+
+            Elements eles = doc.select("tbody > tr");
+            for (Element ele : eles) {
+                GameBean gameBean = new GameBean();
+                String sdf = new SimpleDateFormat(datePattern).format(new Date());
+                gameBean.setUrl(ele.select("td:nth-child(1) > a").attr("href"));
+                gameBean.setGameName(ele.select("td:nth-child(1)").text());
+                gameBean.setGameType(ele.select("td:nth-child(2)").text());
+                gameBean.setUpdateType(ele.select("td:nth-child(3)").text());
+                gameBean.setUpdateTime(ele.select("td:nth-child(5)").text().substring(11));
+                gameBean.setSource("易乐游");
+                gameBean.setHandleFlag("0");
+                gameBean.setCrawlerTime(sdf);
+//                System.out.println(gameBean);
                 Dao.insert(gameBean);
             }
         }
 
         //云更新处理逻辑。
-        if(page.getUrl().regex(YGXHelpUrl).match()){
-            List<String> gameList= page.getHtml().xpath("/html/body/section[3]/table/tbody/tr*").all();
+        if (page.getUrl().regex(YGXHelpUrl).match()) {
+            int totalPage = getYGXTotalPage(page.getHtml().toString());
+            //判断是否是第一次进入处理队列
+            if ("http://www.yungengxin.net.cn/game/updae".equals(page.getUrl().toString())) {
+                //判断页码，如果不止一页，则添加其余页面
+                if (totalPage != 1) {
+                    for (int x = 2; x <= totalPage; x++) {
+                        //http://www.yungengxin.net.cn/game/update?page=2&order=DESC#t-center
+                        String addUrl = "http://www.yungengxin.net.cn/game/update?page=" + x + "&order=DESC#t-center";
+                        page.addTargetRequest(addUrl);
+                    }
+                }
+            }
 
-            for (String temp : gameList){
-//                System.out.println(temp);
-                Document doc = Jsoup.parse(temp);
-                System.out.println(doc.select(""));
+            Document doc = Jsoup.parse(page.getRawText());
+            Elements eles = doc.select("tbody > tr");
+            for (Element ele : eles) {
+                GameBean gameBean = new GameBean();
+                String sdf = new SimpleDateFormat(datePattern).format(new Date());
+                gameBean.setGameName(ele.select("td:nth-child(1)").text());
+                gameBean.setUpdateTime(ele.select("td:nth-child(4)").text().substring(11));
+                gameBean.setGameType(ele.select("td:nth-child(22)").text());
+                gameBean.setGameSize(ele.select("td:nth-child(3)").text());
+                gameBean.setSource("云更新");
+                gameBean.setHandleFlag("0");
+                gameBean.setCrawlerTime(sdf);
+                Dao.insert(gameBean);
             }
         }
     }
@@ -74,14 +127,40 @@ public class processor implements PageProcessor {
 
     public String getUserAgent() {
         String[] list = {
-                "User-Agent,Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36",
-                "User-Agent,Mozilla/5.0 (Windows NT 6.1; rv,2.0.1) Gecko/20100101 Firefox/4.0.1",
-                "User-Agent,Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+                "User-Agent, Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36",
+                "User-Agent, Mozilla/5.0 (Windows NT 6.1; rv,2.0.1) Gecko/20100101 Firefox/4.0.1",
+                "User-Agent, Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
                 "User-Agent, Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)",
                 "User-Agent, Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
                 "User-Agent, Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
         };
         Random rand = new Random();
         return list[rand.nextInt(list.length)];
+    }
+
+    public static int getYGXTotalPage(String temp){
+        try {
+            Pattern p = Pattern.compile("totalPage:\\d+,");
+            Matcher m = p.matcher(temp);
+            m.find();
+            Pattern p1 = Pattern.compile("\\d+");
+            Matcher m1 = p1.matcher(m.group(0));
+            m1.find();
+            return Integer.parseInt(m1.group(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 1;
+        }
+
+    }
+
+    public static int getYLYCurrentPage(String url){
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(url);
+        if (m.find()){
+            return Integer.parseInt(m.group(0));
+        }else {
+            return 0;
+        }
     }
 }
