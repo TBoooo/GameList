41f060484e218633118f9713ebaec4c62890ccfd
booo <yuehaibo@bonc.com.cn>
2019-08-21 10:26:36 +0800

    判断是否写入数据

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,9 @@ public class Main {
         String YGXUrl = "https://yungengxin.com/game/update";
         String YLYUrl = "http://www.yileyoo.com/game/list?page=1";
         Spider spider = Spider.create(new processor());
-//        spider.addUrl(SWUrl);
+        spider.addUrl(SWUrl);
         spider.addUrl(YGXUrl);
-//        spider.addUrl(YLYUrl);
+        spider.addUrl(YLYUrl);
         spider.run();
     }
 }
